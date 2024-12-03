a5c34a82ae7bcde7567e9131fb2c546a03828a24
mike444444444 <onlyjocs@gmail.com>
2024-12-03 10:33:45 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1 class="text-8xl text-center font-black text-emerald-800">{{ title }}</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: "App",
+  data(){
+    return{
+      title: "Hola!!!!🚀"
+    }
+  }
+
+  
+}
+</script>
+<style>
+  
+</style>
